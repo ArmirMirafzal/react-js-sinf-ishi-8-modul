2205b8574725f143bd0ef3db47a747adfd6c36ed
Armir <mirarmirkhan@gmail.com>
2023-06-16 20:35:25 +0500

    index.js changed

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './app';
-import "bootstrap/dist/css/bootstrap.css"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/89-dars-increment/app";
+import "bootstrap/dist/css/bootstrap.css";
 import "./assets/index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App/>
-  </React.StrictMode>
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
 );
-
-
