5f0fa4a0cf27d4e796eea0b323ab81d77bb39ed3
Armir <mirarmirkhan@gmail.com>
2023-06-25 22:48:36 +0500

    loyiha azgina qoldi

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
 import ReactDOM from "react-dom/client";
+
 import App from "./app";
 
-import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/css/normalize.css";
+import "./assets/css/main.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(<App />);
